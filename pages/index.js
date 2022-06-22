8166679aabc67badd05127f0d382ea56480e30c7
mhmadwrekat <mhmmadwreekkat@gmail.com>
2022-06-22 15:40:33 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,39 @@
 import React, { useState, useEffect } from "react";
 import Nav from "../component/Nav";
 import Home from "../component/Home";
+import fetch from "isomorphic-unfetch";
+
+const index = ({ notes }) => {
+  // const [notes, setNotes] = useState();
+  // const get_notes = async () => {
+  //   const resp = await fetch("http://3000/api/notes");
+  //   const { data } = await resp.json();
+  //   setNotes(data);
+  // };
+
+  // useEffect(() => {
+  //   // get_notes();
+  // }, [notes]);
+  console.log("NNNNNNNNN ->> ", notes);
+  return (
+    <React.Fragment>
+      <Nav create={true} />
+      <Home notes={notes} />
+    </React.Fragment>
+  );
+};
+index.getInitialProps = async () => {
+  const resp = await fetch("https://fullstack-snowy.vercel.app/api/notes");
+  const { data } = await resp.json();
+  return {
+    notes: data,
+  };
+};
+export default index;
+/*
+import React, { useState, useEffect } from "react";
+import Nav from "../component/Nav";
+import Home from "../component/Home";
 import axios from "axios";
 
 const index = () => {
@@ -25,3 +58,5 @@ const index = () => {
 };
 
 export default index;
+
+*/
